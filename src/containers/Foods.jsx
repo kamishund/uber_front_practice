636b879a171cf3da99fd9eb92685250d8a067301
kamishund <kamishund@gmail.com>
2021-12-18 23:09:22 +0900

    first commit

--- src/containers/Foods.jsx
+++ src/containers/Foods.jsx
@@ -1,14 +1,27 @@
-import React, { Fragment } from 'react';
+// --- 次にuseEffectを追加 ---
+import React, { Fragment, useEffect } from 'react';
+
+// --- ここから追加 ---
+// apis
+import { fetchFoods } from '../apis/foods';
+// --- ここまで追加 ---
 
 export const Foods = ({
-  match
-}) => {
+    match
+  }) => {
+
+  // --- ここから追加 ---
+  useEffect(() => {
+    fetchFoods(match.params.restaurantsId)
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+  // --- ここまで追加 ---
+
   return (
     <Fragment>
       フード一覧
-      <p>
-      restaurantsIdは {match.params.restaurantsId} です
-      </p>
     </Fragment>
   )
 }
